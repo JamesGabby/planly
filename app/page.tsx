85b8eeef619c66b68ce729cf0eea23e5462cd77e
JamesGabby <jlghfe36@gmail.com>
2026-01-28 07:33:25 +0000

    remove bdc

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
 import { NavbarLanding } from "@/components/navbar-landing";
 import { Footer } from "@/components/footer";
 import LandingPage from "./landing/page";
-import BirthdayCard from "@/components/BirthdayCard";
 
 export default function Home() {
   return (
     <>
       <NavbarLanding />
-      <BirthdayCard />
       <LandingPage />
       <Footer />
     </>
